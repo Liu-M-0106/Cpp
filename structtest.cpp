e367f8f027a879638ddac982775166a008e82cf8
LiuM <liumx0106@gmail.com>
2025-11-02 20:53:56 +0800

    modified:   "Cpp\345\237\272\347\241\200.md" Notice: 1.如何通过指针访问结构体变量，以及指针的for循环 2.通过函数初始化struct的方法及注意事项

---
+++ structtest.cpp
@@ -0,0 +1,57 @@
+// #include <iostream>
+// #include <string>
+// using namespace std;
+
+// // 定义一个学生结构体
+// struct Student {
+//     int id;         // 学号
+//     string name;    // 姓名
+//     float score;    // 成绩
+// };
+
+// int main() {
+//     Student students[] = {
+//         {101, "张三", 92.5f},
+//         {102, "李四", 88.0f},
+//         {103, "王五", 95.3f}
+//     };
+//     int count = sizeof(students) / sizeof(students[0]);  // 计算数组长度
+
+//     for (Student *p= students; p<students+count; p++) {
+//         //     // 通过指针访问结构体成员（使用 -> 运算符）
+//         cout << "学号：" << p->id << endl;       // 指针访问id
+//         cout << "姓名：" << p->name << endl;     // 指针访问name
+//         cout << "成绩：" << p->score << endl;    // 指针访问score
+//         cout << "---------------------" << endl;
+//     }
+
+//     return 0;
+// }
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct student {
+  int num;
+  string name;
+  float grade;
+};
+
+
+int main() {
+// 注意初始化结构体方法
+  student students[3] {
+    {1, "张三", 55.6},
+    {2, "Kusa", 88.9},
+    {3,"kda",555.8}
+  };
+  int count = sizeof(students) / sizeof(students[0]);
+  for (auto p= students; p < students + count; p++) {
+    cout << p->num  << endl;
+    cout << p->name << endl;
+    cout << p->grade<<endl;
+  }
+
+
+  return 0;
+}
