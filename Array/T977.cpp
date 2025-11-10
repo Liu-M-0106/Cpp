2ce7f5d59322f554cac6db37314b3ce1403db6ed
LiuM <liumx0106@gmail.com>
2025-11-10 22:33:02 +0800

    modified:   Array/T977.cpp 	modified:   "\347\256\227\346\263\225.md" 	modified:   "\351\227\256\351\242\230\346\200\273\347\273\223.md"

--- Array/T977.cpp
+++ Array/T977.cpp
@@ -29,9 +29,44 @@ using namespace std;
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
-        
+      int fast = nums.size() - 1, slow = 0;
+      vector<int> nums1 = nums;
+      int i = fast;
+      while(slow<=fast){
+      if (nums[fast] * nums[fast] <= nums[slow] * nums[slow]) {
+        nums1[i] = nums[slow]*nums[slow];
+        slow++;
+      }
+      else {
+        nums1[i] = nums[fast]*nums[fast];
+        fast--;
+      }
+      i--;
+      }
+      return nums1;
     }
 };
+
+// 代码随想录题解
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& A) {
+        int k = A.size() - 1;
+        vector<int> result(A.size(), 0);
+        for (int i = 0, j = A.size() - 1; i <= j;) { // 注意这里要i <= j，因为最后要处理两个元素
+            if (A[i] * A[i] < A[j] * A[j])  {
+                result[k--] = A[j] * A[j];
+                j--;
+            }
+            else {
+                result[k--] = A[i] * A[i];
+                i++;
+            }
+        }
+        return result;
+    }
+};
+
 
 
 int main() {
